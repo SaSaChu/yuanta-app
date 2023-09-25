ab7c618a3a7e664f33d5f1f590ed77d255a008d4
CHO <bsuysheep927@gmail.com>
2023-09-25 21:31:47 +0800

    0925 hank 自製行業別下拉選單樣式

--- js/page02.js
+++ js/page02.js
@@ -3,10 +3,9 @@ $(function() {
 	
 	sectionId.on('shown.bs.offcanvas', function () {
 		$('.list-group-item').click(function() {
-			$('.industrySelect option[selected]').text($(this).text())
+			$('.industrySpan').text($(this).text())
 			$('input[name="industryInput"]').val($(this).attr('data-value'))
 			sectionId.offcanvas('hide');	
 		})
 	});
-	
 });
