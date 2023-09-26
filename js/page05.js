c88daf401f78fee29eebb2b79998ea2ba8ea2fc7
CHO <bsuysheep927@gmail.com>
2023-09-26 20:26:01 +0800

    0926 hank js 獨立為 page05.js、調整註解

---
+++ js/page05.js
@@ -0,0 +1,19 @@
+$(function() {
+	// 20230926 要增加或減少修改(n+4)的數字即可，預設值有改的話 css 也要修改 -->
+
+	$('.list-toggle').click(function() {
+		if($('.list-group-item').is(':hidden')){
+			$('.list-group-item:nth-of-type(n+4)').slideDown(500, function() {
+				$(this).css({'display':'block'}); 
+			})
+			$(this).find('.list-toggle-text').text('收合');
+			$(this).find('i').removeClass('bi-chevron-down').addClass('bi-chevron-up');
+		} else {
+			$('.list-group-item:nth-of-type(n+4)').slideUp(500)
+			$(this).find('.list-toggle-text').text('查看更多');
+			$(this).find('i').removeClass('bi-chevron-up').addClass('bi-chevron-down');
+		}
+		
+	});
+          
+});
